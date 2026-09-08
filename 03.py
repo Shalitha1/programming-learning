d10acae6b2717a6c76e7391ccdee50400bc4e658
Shalitha Randika Rathnayaka <shalitharathnayaka@gmail.com>
2026-09-08 18:33:35 +0530

    d

---
+++ 03.py
@@ -0,0 +1,19 @@
+# numbers = [1,2,3,4,5,6,7,200]
+
+# largest = numbers[0]
+
+# for num in numbers:
+#     if num > largest:
+#         largest = num
+
+# print(largest)
+
+numbers = [-9,-3,-3,-1,-12]
+
+largest = numbers[0]
+
+for i in numbers:
+    if i > largest:
+        largest = i
+
+print(largest)
