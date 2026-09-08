55198b285d8a8b3f80e8c2a630ee2b3608fdb1d0
Shalitha Randika Rathnayaka <shalitharathnayaka@gmail.com>
2026-09-08 18:39:40 +0530

    done for today

--- 05.py
+++ 05.py
@@ -1,14 +1,24 @@
-n = int(input("Enter a number:"))
+# n = int(input("Enter a number:"))
+
+# for i in range(1, n+1):
+#     if i % 3 == 0 and i % 5 == 0:
+#         print("FizzBuzz")
+#     elif i % 3 ==0:
+#         print("Fizz")
+#     elif i % 5 == 0:
+#         print("Buzz")
+#     else:
+#         print(i)
+
+n = int(input("Enter a value:"))
 
 for i in range(1, n+1):
     if i % 3 == 0 and i % 5 == 0:
         print("FizzBuzz")
-    elif i % 3 ==0:
-        print("Fizz")
-    elif i % 5 == 0:
+    elif i % 3 == 0:
         print("Buzz")
-    else:
-        print(i)
-
+    elif i % 5 == 0:
+        print("Fizz")
+    else: print(i)
 
 
