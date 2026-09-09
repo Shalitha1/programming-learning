cc55a9c94d818d4ba89f1020520bb89d66e6f3cb
Shalitha Randika Rathnayaka <shalitharathnayaka@gmail.com>
2026-09-09 13:34:37 +0530

    day 02

--- 05.py
+++ 05.py
@@ -10,15 +10,16 @@
 #     else:
 #         print(i)
 
+
+
+
 n = int(input("Enter a value:"))
 
-for i in range(1, n+1):
-    if i % 3 == 0 and i % 5 == 0:
+for num in range(1, n+1):
+    if num % 3 == 0 and num % 5 == 0:
         print("FizzBuzz")
-    elif i % 3 == 0:
-        print("Buzz")
-    elif i % 5 == 0:
+    elif num % 3 == 0:
         print("Fizz")
-    else: print(i)
-
-
+    elif num % 5 == 0:
+        print("Buzz")
+    else: print("num")
